f51265ac664bcf2fa87a7c1d95e00a184b43d07a
Roque Laecio dos Santos <rlaecio@hotmail.com>
2020-10-08 01:14:24 +0100

    refactoring

--- main.go
+++ main.go
@@ -2,45 +2,33 @@ package main
 
 import (
 	"fmt"
+	"banco/contas"
 )
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string{
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo 
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente)Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "deposito realizado com sucesso", c.saldo
-	} else {
-		return "Valor do deposito menos que zero", c.saldo
-	}
-}
 
 func main() {
-	contaDaCris := ContaCorrente{}
-	contaDaCris.titular = "Cris"
-	contaDaCris.saldo = 500
-	contaDaCris.numeroAgencia = 358
-	contaDaCris.numeroConta = 123457
+
+	contaDaCris := contas.ContaCorrente{}
+	contaDaCris.Titular = "Cris"
+	contaDaCris.Saldo = 500
+	contaDaCris.NumeroAgencia = 358
+	contaDaCris.NumeroConta = 123457
 	
 
-	fmt.Println(contaDaCris.saldo)
+	fmt.Println(contaDaCris.Saldo)
 	fmt.Println(contaDaCris.Sacar(300))
-	fmt.Println(contaDaCris.saldo)
+	fmt.Println(contaDaCris.Saldo)
 	fmt.Println(contaDaCris.Depositar(400))
+
+	// -----------------------
+	contaDaSilvia := contas.ContaCorrente{Titular:"Silvia", Saldo: 300}
+    contaDoGustavo := contas.ContaCorrente{Titular:"Gustavo", Saldo: 100}
+    fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
+	
+	status := contaDaSilvia.Transferir(200, &contaDoGustavo)
+	fmt.Println(status)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
 
 }
